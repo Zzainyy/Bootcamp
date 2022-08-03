c2b77e3d8313a346a8fa0e958ab71e92d3574c72
Zainy <105654332+Zzainyy@users.noreply.github.com>
2022-08-03 18:31:12 +0100

    Add files via upload

---
+++ my portfolio/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'body': '#f8f5f2',
+        'selected text': '#232323',
+        'theme': '#3F3FFF',
+        'nav': '#078080',
+        'secondary': '#f45d48',
+        'badge': 'gray',
+        'input border': '#feefe8',
+        'input': '#222525',
+      },
+      fontFamily: {
+        'spectral': ["'Spectral'", 'serif'] 
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+
+}
